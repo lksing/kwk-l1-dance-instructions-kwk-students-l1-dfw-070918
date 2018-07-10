a2f56f45417f790275e7debbd7e59dec34bcbc20
lksing <leiaxsing@gmail.com>
2018-07-10 14:28:15 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,60 @@
 
 # Code your methods below
+
+def starting_stance
+  puts "Plant legs far apart" 
+end
+
+def bust_a_move
+  puts "Be cool and be snazzy!"
+end
+
+def base_footwork
+   puts "Lift right foot to the ground"
+   puts "Return right foot"
+   puts "Finishing with a small skip-step backward"
+ end
+
+def skip_step(left)
+  puts "Lower"
+  puts "Bounce"
+  puts "Briefly"
+end
+
+ def bob_the_reins
+   puts "Do Holding your arms out in front of you, straight and at chest level"
+   puts "Cross your right wrist over your left and hold them together"
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song."
+ end
+
+def lasso
+puts "Do Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left"
+puts "near your chin, pointing your left elbow straight left."
+puts "Lift right arm up and point right elbow diagonally to the right."
+puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso."
+end
+
+# puts starting_stance
+# puts bust_move
+# puts footwork
+# puts bob_the_reins
+# puts skip_step("left")
+# puts skip_step("left")
+# puts skip_step("left")
+# puts skip_step("left")
+# puts skip_step("left")
+# puts skip_step("left")
+# puts skip_step("left")
+# puts skip_step("left")
+# puts lasso
+# puts skip_step("left")
+# puts skip_step("left")
+# puts skip_step("left")
+# puts skip_step("left")
+# puts skip_step("left")
+# puts skip_step("left")
+# puts skip_step("left")
+# puts skip_step("left")
+
+
+
